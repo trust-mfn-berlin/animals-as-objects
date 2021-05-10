1ac483f2b2e51d0c76ed5974dcb3b6d99e8f614a
Son La Pham <hello@son-la.co>
2021-05-10 17:23:22 +0200

    audio and video test

---
+++ components/sound.vue
@@ -0,0 +1,18 @@
+<template>
+  <audio controls="controls">
+    <source type="audio/mp3" :src="file">
+    Your browser does not support the audio element.
+  </audio>
+</template>
+
+<script>
+export default {
+  name:'audiocomponent',
+  props:{
+    file:{
+      type:String,
+      required:true
+    }
+  }
+}
+</script>
