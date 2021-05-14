113295f4c859db2f4ddb0cfedd5e68db8d1289eb
Son La Pham <hello@son-la.co>
2021-05-14 18:05:51 +0200

    updated test

--- components/graph.vue
+++ components/graph.vue
@@ -7,6 +7,9 @@
 
 // https://github.com/d3/d3-zoom
 
+  // Variables tester
+// https://bl.ocks.org/steveharoz/8c3e2524079a8c440df60c1ab72b5d03
+
 <template>
   <div id="d3"></div>
 </template>
@@ -30,28 +33,28 @@ export default {
         }
       },
       nodes : [
-        { id: 'Lorem' },
-        { id: 'Ipsum' },
-        { id: 'Dolor' },
-        { id: 'Sit' },
-        { id: 'Amet' },
-        { id: 'Buzz' },
-        { id: 'Wup' },
-        { id: 'Testtesttest' },
-        { id: '123' }
+        { slug: 'Lorem', frontmatter:{ title: 'Lorem' } },
+        { slug: 'Ipsum', frontmatter:{ title: 'Ipsum' } },
+        { slug: 'Dolor', frontmatter:{ title: 'Dolor' } },
+        { slug: 'Sit', frontmatter:{ title: 'Sit' } },
+        { slug: 'Amet', frontmatter:{ title: 'Amet' } },
+        { slug: 'Buzz', frontmatter:{ title: 'Buzz' } },
+        { slug: 'Wup', frontmatter:{ title: 'Wup' } },
+        { slug: 'Testtesttest', frontmatter:{ title: 'Testtesttest' } },
+        { slug: '123', frontmatter:{ title: '123' } }
       ],
       links : [
-        { source: 'Lorem', target: 'Sit', relation: 'a', value: 1.3 },
+        { source: 'Lorem', target: 'Sit', relation: 'a', value: 4 },
         { source: 'Sit', target: 'Amet', relation: 'b', value: 1 },
         { source: 'Sit', target: 'Buzz', relation: 'c', value: 1 },
         { source: 'Sit', target: '123', relation: 'd', value: 1 },
-        { source: 'Ipsum', target: 'Buzz', relation: 'e', value: 2 },
-        { source: 'Wup', target: 'Amet', relation: 'f', value: 0.9 },
+        { source: 'Ipsum', target: 'Buzz', relation: 'e', value: 1 },
+        { source: 'Wup', target: 'Amet', relation: 'f', value: 2 },
         { source: 'Testtesttest', target: 'Sit', relation: 'g', value: 1 },
         { source: 'Amet', target: 'Testtesttest', relation: 'h', value: 1.6 },
         { source: 'Dolor', target: '123', relation: 'i', value: 0.7 },
-        { source: 'Buzz', target: 'Wup', relation: 'j', value: 2 }
-      ]
+        { source: 'Buzz', target: 'Wup', relation: 'j', value: 3 }
+      ],
     }
   },
   methods:{
@@ -66,29 +69,29 @@ export default {
         .attr("width", this.attr.width)
         .attr("height", this.attr.height);
 
-      const simulation = d3.forceSimulation(graphdata.nodes)
-        .force("link", d3.forceLink(graphdata.links).id(d => d.slug))
-        .force("charge", d3.forceManyBody())
+      const simulation = d3.forceSimulation(this.nodes)
+        .force("link", d3.forceLink(this.links).id(d => d.slug).distance(100))
+        .force("charge", d3.forceManyBody().strength(-1200).distanceMin(1).distanceMax(2000))
         .force("center", d3.forceCenter(this.attr.width / 2, this.attr.height / 2))
-        .force("radial", d3.forceRadial(500))
-        .force("collide", d3.forceCollide(20))
+        // .force("radial", d3.forceRadial(500))
+        .force("collide", d3.forceCollide(80).strength(0.2))
 
       let colorScale = d3.scaleOrdinal()
-        .domain(d3.range(graphdata.nodes.length))
-        .range(d3.schemeCategory10)
+        .domain(d3.range(this.nodes.length))
+        .range(["#84E3B0","#FA9129","#C64C4C","#EE9389","#9A7051","#0047FF","#DD3821","#6E2E60","#F5C721"])
 
       const link = svg.append("g")
-        .attr("stroke", "#eee")
-        .attr("stroke-opacity", 0.6)
+        .attr("stroke", "#fff")
+        .attr("stroke-opacity", 1)
         .selectAll("line")
-        .data(graphdata.links)
+        .data(this.links)
         .join("line")
-        .attr("stroke-width", d => Math.sqrt(d.value));
+        .attr("stroke-width", d => d.value);
 
       const node = svg.append("g")
         
         .selectAll("circle")
-        .data(graphdata.nodes)
+        .data(this.nodes)
         .enter()
         .append('g')
         .attr('transform', function (d) {
@@ -102,19 +105,17 @@ export default {
         .call(this.drag(simulation));
 
         node.append("circle")
-          .attr("stroke", "#fff")
-          .attr("stroke-width", 1.5)
-          .attr("r", 4.5)
+          .attr("r", 45)
           .attr('fill', function (d, i) {
             return colorScale(i)
           })
 
         node.append("text")
           
-          .attr("dy", "14px")
+          .attr("dy", "5px")
           .attr("font-family", "Optician Sans")
           .style("font-size", "10px")
-          .attr("fill", "#ccc")
+          .attr("fill", "#fff")
           .attr("letter-spacing", '0.1em')
           .text(function(d) { return d.frontmatter.title })
           .attr("dx", function(d) { return this.getBoundingClientRect().width/2*-1})
@@ -128,7 +129,7 @@ export default {
             d3.select(this)
               .transition()
               .duration(200)
-              .attr("fill", "#ccc")
+              .attr("fill", "#fff")
           });
 
           node.on('mouseover', function(event, d) {
@@ -137,7 +138,7 @@ export default {
                 return "black";
               }
               else {
-                return "#eee";
+                return "#fff";
               }
               });
           });
@@ -148,7 +149,7 @@ export default {
 
           // Set the stroke width back to normal when mouse leaves the node.
           node.on('mouseout', function() {
-            link.style('stroke', "#eee");
+            link.style('stroke', "#fff");
           });
 
         
@@ -178,7 +179,7 @@ export default {
       }
       
       function dragended(event) {
-        if (!event.active) simulation.alphaTarget(0);
+        if (!event.active) simulation.alphaTarget(0.0001);
         event.subject.fx = null;
         event.subject.fy = null;
       }
--- pages/index.vue
+++ pages/index.vue
@@ -26,5 +26,7 @@ export default {
 </script>
 
 <style>
-
+body{
+  background-color: #F3EEE9;
+}
 </style>
