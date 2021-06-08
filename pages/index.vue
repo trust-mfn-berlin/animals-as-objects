b8827c52b0f360b383c602249e4e05f74d89775a
Son La Pham <hello@son-la.co>
2021-06-08 16:32:15 +0200

    no identity widget

--- pages/index.vue
+++ pages/index.vue
@@ -51,7 +51,7 @@ export default {
   },
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
+      // script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     };
   },
 }
