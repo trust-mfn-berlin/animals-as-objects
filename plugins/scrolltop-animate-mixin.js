02715f4cc3c03e6be90ff1c3cbb476726cdbae40
Son La Pham <hello@son-la.co>
2021-06-09 12:16:06 +0200

    add extra functions

--- plugins/scrolltop-animate-mixin.js
+++ plugins/scrolltop-animate-mixin.js
@@ -1,30 +1,65 @@
+// https://github.com/Robbendebiene/Sliding-Scroll
+
 import Vue from "vue"
 
 if (!Vue.scrollTopMixin) {
-  Vue.scrollTopMixin = true
-  Vue.mixin({
-    methods:{
-      scrollToTop (duration) {
-        // cancel if already on top
-        if (document.scrollingElement.scrollTop === 0) return;
-      
-        const cosParameter = document.scrollingElement.scrollTop / 2;
-        let scrollCount = 0, oldTimestamp = null;
-      
-        function step (newTimestamp) {
-            if (oldTimestamp !== null) {
-                // if duration is 0 scrollCount will be Infinity
-                scrollCount += Math.PI * (newTimestamp - oldTimestamp) / duration;
-                if (scrollCount >= Math.PI) return document.scrollingElement.scrollTop = 0;
-                document.scrollingElement.scrollTop = cosParameter + cosParameter * Math.cos(scrollCount);
-            }
-            oldTimestamp = newTimestamp;
-            window.requestAnimationFrame(step);
-        }
-        window.requestAnimationFrame(step);
-      }
+Vue.scrollTopMixin = true
+Vue.mixin({
+methods:{
+  /*
+ * y: the y coordinate to scroll, 0 = top
+ * duration: scroll duration in milliseconds; default is 0 (no transition)
+ * element: the html element that should be scrolled ; default is the main scrolling element
+ */
+  scrollToY (y, duration = 0, element = document.scrollingElement) {
+  // cancel if already on target position
+  if (element.scrollTop === y) return;
+
+  const cosParameter = (element.scrollTop - y) / 2;
+  let scrollCount = 0, oldTimestamp = null;
+
+  function step (newTimestamp) {
+    if (oldTimestamp !== null) {
+      // if duration is 0 scrollCount will be Infinity
+      scrollCount += Math.PI * (newTimestamp - oldTimestamp) / duration;
+      if (scrollCount >= Math.PI) return element.scrollTop = y;
+      element.scrollTop = cosParameter + y + cosParameter * Math.cos(scrollCount);
     }
-  }) // Set up your mixin then
+    oldTimestamp = newTimestamp;
+    window.requestAnimationFrame(step);
+  }
+  window.requestAnimationFrame(step);
+  },
+
+  /*
+  * duration: scroll duration in milliseconds; default is 0 (no transition)
+  * this function is using the scrollToY function
+  */
+  scrollToTop(duration = 0) {
+    scrollToY(0, duration, document.scrollingElement);
+  },
+
+  /*
+  * id: the id of the element as a string that should be scrolled to
+  * duration: scroll duration in milliseconds; default is 0 (no transition)
+  * this function is using the scrollToY function on the main scrolling element
+  */
+  scrollToId(id, duration = 0) {
+    const offset = Math.round(document.getElementById(id).getBoundingClientRect().top);
+    scrollToY(document.scrollingElement.scrollTop + offset, duration);
+  },
+
+  /*
+  * element: the node object that should be scrolled to
+  * duration: scroll duration in milliseconds; default is 0 (no transition)
+  * this function is using the scrollToY function on the main scrolling element
+  */
+  scrollToElement(element, duration = 0) {
+    const offset = Math.round(element.getBoundingClientRect().top);
+    scrollToY(document.scrollingElement.scrollTop + offset, duration);
+  }
+}
+})
 }
 
 
