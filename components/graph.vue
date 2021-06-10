effa43a7ee674c2b2f079cf5be9069fec466188c
Son La Pham <hello@son-la.co>
2021-06-10 15:29:41 +0200

    setheight css

--- components/graph.vue
+++ components/graph.vue
@@ -11,7 +11,7 @@
 // https://bl.ocks.org/steveharoz/8c3e2524079a8c440df60c1ab72b5d03
 
 <template>
-  <div id="d3"></div>
+  <div id="d3" :style="{height: attr.height + 'px'}"></div>
 </template>
 
 <script>
