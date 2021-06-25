b86aa412692aedbcb2e1b7de1807b54598396694
Son La Pham <hello@son-la.co>
2021-06-25 12:07:08 +0200

    routes use SVG

--- components/your-current-route.vue
+++ components/your-current-route.vue
@@ -3,6 +3,11 @@
     <li v-if="routes.length > max" class="excess">+{{indexStart}}</li>
     <li v-for="(route, index) in routes.slice(indexStart,routes.length)" :key="index">
       <Inline :article="route" />
+      <svg width="26" height="26" viewBox="0 0 26 26" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M12.7988 24.9829L24.8989 12.8829L12.7988 0.782849" stroke="black"/>
+        <line x1="24.6113" y1="12.8901" x2="1.86801" y2="12.8901" stroke="black"/>
+      </svg>
+      <!-- <span class="dots"></span> -->
     </li>
   </ol>
 </template>
@@ -44,6 +49,34 @@ ol{
   border-radius: @radius-l;
 }
 
+li{
+  display: flex;
+  align-items: center;
+  &:last-of-type{
+    svg{
+      display: none;
+    }
+    .dots{
+      display: none;
+    }
+  }
+}
+
+.dots{
+  width: 0.5rem;
+  height: 1px;
+  border-bottom: 1px dashed @black;
+  margin-left:0.5rem;
+}
+
+svg{
+  display: inline-block;
+  margin-left:0.5rem;
+  height: 1rem;
+  // background-color: blue;
+  
+}
+
 li.excess{
   font-size:@fs-s;
   background-color: @white;
@@ -56,13 +89,13 @@ li.excess{
 
 }
 
-li a::before{
-  display: inline-block;
-  // content:'\a0,'
-  content:'→\a0'
-}
+// li a::before{
+//   display: inline-block;
+//   // content:'\a0,'
+//   content:'→\a0'
+// }
 
-li:first-of-type a::before{
-  content:''
-}
+// li:first-of-type a::before{
+//   content:''
+// }
 </style>
