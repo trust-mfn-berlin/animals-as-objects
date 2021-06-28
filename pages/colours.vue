f0d8f168815ae4a8e9ad78e5e361107d8871cbbc
Son La Pham <hello@son-la.co>
2021-06-28 16:16:54 +0200

    colour update

--- pages/colours.vue
+++ pages/colours.vue
@@ -1,6 +1,6 @@
 <template>
   <ol>
-    <li v-for="i in 15" :key="i" :style="{backgroundColor:'var(--scheme-'+(i-1)+'-bg)', color:'var(--scheme-'+(i-1)+'-fg)'}">Colour Scheme #{{i-1}}</li>
+    <li v-for="i in 16" :key="i" :style="{backgroundColor:'var(--scheme-'+(i-1)+'-bg)', color:'var(--scheme-'+(i-1)+'-fg)'}">Colour Scheme #{{i-1}}</li>
   </ol>
 </template>
 
