3ae87c471c0c4ee8a75e4a2d42b60d979d64d3a8
Son La Pham <hello@son-la.co>
2021-06-30 18:35:32 +0200

    adjust height

--- components/article-map.vue
+++ components/article-map.vue
@@ -1,12 +1,14 @@
 <template>
-  <article :style="{minHeight: height+'px'}">
+  <article :style="{minHeight: calcHeight + 'px'}">
     <div class="slide" v-for="(slide, index) in slides" :key="index" @click="nextSlide">
+      <transition name="fade">
       <figure v-show="currentSlide == index">
         <nuxt-img width="1000" :height="height" :src="slide.map_image" /> 
-        <figcaption class="f-serif quote">
+        <figcaption ref="caption" class="quote">
         {{slide.caption}}
         </figcaption>
       </figure>
+      </transition>
     </div>
   </article>
 </template>
@@ -22,9 +24,13 @@ export default {
   },
   data(){
     return{
-      height:500,
+      height:700,
+      calcHeight: this.height,
       currentSlide:0
     }
+  },
+  computed:{
+
   },
   methods:{
     nextSlide(){
@@ -33,7 +39,16 @@ export default {
       } else {
         this.currentSlide = 0
       }
+    },
+    calculateHeight(){
+      this.calcHeight = this.height + this.$refs.caption[this.currentSlide].offsetHeight
     }
+  },
+  mounted(){
+    this.calculateHeight();    
+  },
+  updated(){
+    this.calculateHeight(); 
   }
 }
 </script>
@@ -43,6 +58,8 @@ article{
   width: 100%;
   position: relative;
   margin: 4rem 0;
+
+  .animatemedium(all);
 
   div.slide{
     width: 100%;
@@ -54,13 +71,11 @@ article{
         width: 100%;
       }
       figcaption{
-        background-color: rgba(255,255,255,0.9);
+        // background-color: rgba(255,255,255,0.9);
+        margin: 0;
+        background-color: @bg-2;
         padding: @space-m;
         width:100%;
-        // position: absolute;
-        bottom:0;
-        // margin: -@space-m auto 0 auto;
-        // z-index: 2;
       }
     }
 
