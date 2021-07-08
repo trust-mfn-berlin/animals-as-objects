797e633aeb13edeec5e4bc96d98181628ed69803
Son La Pham <hello@son-la.co>
2021-07-08 11:42:59 +0200

    unique ids + show timeline

--- pages/articles.vue
+++ pages/articles.vue
@@ -7,7 +7,7 @@
       <ol>
         <template v-for="(letter, index) in articlesAlphabetical">
           <h2 :key="index">{{index}}</h2>
-          <Inline v-for="article in letter" :key="article.id" :article="article" />
+          <Inline v-for="article in letter" :key="article.slug" :article="article" />
         </template>
       </ol>
       </section>
@@ -15,21 +15,21 @@
       <section v-show="activeFilter == 'type'">
       <h2>Theme</h2>
       <ul>
-        <li v-for="article in articlesType.theme" :key="article.id">
+        <li v-for="article in articlesType.theme" :key="article.slug">
           <Inline :article="article"/>
         </li>
       </ul>
 
       <h2>Story</h2>
       <ul>
-        <li v-for="article in articlesType.story" :key="article.id">
+        <li v-for="article in articlesType.story" :key="article.slug">
           <Inline :article="article"/>
         </li>
       </ul>
 
       <h2>Material</h2>
       <ul>
-        <li v-for="article in articlesType.material" :key="article.id">
+        <li v-for="article in articlesType.material" :key="article.slug">
           <Inline :article="article"/>
         </li>
       </ul>
@@ -95,6 +95,7 @@ export default {
   },
   created(){
     this.$store.commit('setSiteLanguage', 'en')
+    this.$store.commit('setActiveFilter', 'date')
   },
 }
 </script>
