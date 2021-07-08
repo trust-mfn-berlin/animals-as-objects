175ad2ad1b938fb9f63ca2432dcc8bdbb80db7ec
Son La Pham <hello@son-la.co>
2021-07-08 10:55:50 +0200

    styling

--- components/cookie-bar.vue
+++ components/cookie-bar.vue
@@ -1,14 +1,15 @@
 <template>
   <form id='cookie-disclaimer' v-show="!this.$store.getters.isTrackingEnabled">
-    <label class='text-links f-mono'>Accept Cookies? read our <nuxt-link to="/datenschutz">privacy policy</nuxt-link></label>
-    <button @click="onAccept">Yes</button>
+    <label class='text-links f-mono subheading'>Allow Cookies? read our <nuxt-link to="/datenschutz">privacy policy</nuxt-link></label>
+    <text-button type="button" @click.native="onAccept">Accept</text-button>
   </form>
 </template>
 
 <script>
 import { v4 as uuidv4 } from 'uuid';
 
-export default {
+
+export default{
   name:'cookie-bar',
   methods:{
     onAccept(e){
@@ -32,9 +33,20 @@ export default {
   position: fixed;
   bottom: @space-s;
   left:@space-s;
-  background: @white;
-  padding:@space-s;
+  // background: @white;
+  // padding:@space-s;
   z-index: @z-cookie;
-  box-shadow: @shadow;
+  // box-shadow: @shadow;
+
+  label{
+    background-color: @white;
+    box-shadow: @shadow;
+    // display: inline-block;
+    padding: 0.625rem 0.8rem 0.7rem;
+    border-radius: @radius-s;
+    width: auto;
+    position: relative;
+    height: 2rem;
+  }
 }
 </style>
