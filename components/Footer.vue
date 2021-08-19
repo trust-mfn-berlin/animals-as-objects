afeb8a57900bb1014aa1659c268a7a29b65deb82
Son La Pham <hello@son-la.co>
2021-08-19 15:48:58 +0200

    footer bilingual

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,7 +1,12 @@
 <template>
   <footer>
-    <small class="f-mono">Animals as Objects: a project by the Museum für Naturkunde and Zoo Berlin</small>
-    <small class="f-mono"><nuxt-link to='/datenschutz'>Datenschutz</nuxt-link> <nuxt-link to='/imprint'>Imprint</nuxt-link></small>
+    <small class="f-mono" v-if="$store.getters.siteLanguage == 'de'">
+      <em>Tiere als Objekte?</em> <span class="mob-hidden">Eine Website des Forschungsprojekts "Tiere als Objekte. Zoologische Gärten und Naturkundemuseum Berlin, 1810 bis 2020"</span>
+    </small>
+    <small class="f-mono" v-else>
+      <em>Animals as Objects?</em> <span class="mob-hidden">A website by the research project "Animals as Objects. Zoological Gardens and Natural History Museum Berlin, 1810 to 2020"</span>
+    </small>
+    <small class="f-mono"><nuxt-link to='/datenschutz'>{{$store.getters.siteLanguage == "de" ? 'Datenschutzerklärung' : 'Data privacy policy'}}</nuxt-link> <nuxt-link to='/imprint'>Imprint</nuxt-link></small>
     
     <!-- <icon-button icon="arrow-up" tao_type="material" @click.native="scrollToY(0, 800)"></icon-button> -->
   </footer>
