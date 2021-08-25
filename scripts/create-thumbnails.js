4a6ac4fd6e8c255dda4fa93573db5b30e7ea9e88
Son La Pham <hello@son-la.co>
2021-08-25 16:24:52 +0200

    dont convert non-cover images

--- scripts/create-thumbnails.js
+++ scripts/create-thumbnails.js
@@ -23,17 +23,22 @@ function getFileNames(dir, encoding = "utf-8", withFileTypes = true) {
   try {
       fs.readdirSync(dir).forEach(File => {
       const absolute = path.join(dir, File);
-      names.push(absolute)
+      // console.log(File);
+      const relative = File;
+      names.push({absolute, relative});
     });
   } catch (e) {
       console.log("e", e);
   }
   for (let i = 0; i < names.length; i++) {
-    const file = names[i];
+    const file = names[i].absolute;
+    const filename = names[i].relative;
     if(path.extname(file) == ".jpg" || path.extname(file) == ".png" || path.extname(file) == ".jpeg"){
-      // if(path)
-      console.log(file);
-      imageFiles.push(file);
+
+      if(filename.slice(0,5) == "cover"){ //Speed up generation by only converting files starting with cover-
+        imageFiles.push(file);
+      }
+      
     }
   }
   return imageFiles;
@@ -48,7 +53,7 @@ function getFileNames(dir, encoding = "utf-8", withFileTypes = true) {
   if(imageQueue.length == 0){
     console.log('No images to resize');
   } else {
-    console.log("Images to resize", imageQueue.length, imageQueue);
+    console.log("Images to resize", imageQueue.length);
   }
 
   // Do the reading and resizing
@@ -77,7 +82,7 @@ function getFileNames(dir, encoding = "utf-8", withFileTypes = true) {
 
 
       img.cover(sizeSmall, sizeSmall);
-      console.log('Old size', "W", w, "H", h);
+      // console.log('Old size', "W", w, "H", h);
       console.log('New size', "W", img.bitmap.width, "H", img.bitmap.height);
       
       img.write(newPathSmall);
@@ -85,7 +90,7 @@ function getFileNames(dir, encoding = "utf-8", withFileTypes = true) {
 
     });    
     
-    console.log('Finished creating thumbnails');
+    // console.log('Finished creating thumbnails');
   }
 
 })();
